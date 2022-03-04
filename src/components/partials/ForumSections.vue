47fd6ad68b9dca25307ee4794d82e095e696da9b
katiepalumbo <palumbo.katie@gmail.com>
2022-03-04 08:26:00 +0100

    reso forum sections dinamico

--- src/components/partials/ForumSections.vue
+++ src/components/partials/ForumSections.vue
@@ -1,74 +1,13 @@
 <template>
     <div class="forum-sections-container">
         <div class="col-8 m-auto sections_grid m-5">
-            <div class="row">
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
+            <div class="row row-cols-4 icon-row g-4">
+                <div class="col section_card" v-for="(section, index) in forumSections" :key="index">
+                    <div class="icon">
+                        <i :class= "`${section.prefix}${section.type} ${section.prefix}${section.family}`" class="icon_image"></i>
+                        <p class="title">{{section.name}}</p>
                     </div>
                 </div>
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-                <div class="col section_card">
-                    <div>
-                        icon
-                    </div>
-                    <div>
-                        title
-                    </div>
-                </div>
-
 
             </div>
 
@@ -83,68 +22,77 @@ export default {
     name: 'ForumSections',
     data() {
         return{
+            content: '',
             forumSections: [
             {
-                name: 'cat',
+                //<i class="fa-solid fa-file-lines"></i>
+                name: 'News and Community',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'file-lines',
+                family: 'solid',
                 color: 'orange'
             },
             {
-                name: 'crow',
+                //<i class="fa-brands fa-apple"></i>
+                name: 'Apple Forum',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'apple',
+                family: 'brands',
                 color: 'orange'
             },
             {
-                name: 'dog',
+                //<i class="fa-brands fa-windows"></i>
+                name: 'Microsoft Forum',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'windows',
+                family: 'brands',
                 color: 'orange'
             },
             {
-                name: 'dove',
+                //<i class="fa-brands fa-android"></i>
+                name: 'Android Forum',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'android',
+                family: 'brands',
                 color: 'orange'
             },
             {
-                name: 'dragon',
+                //<i class="fa-solid fa-mug-saucer"></i>
+                name: 'General Discussion',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'mug-saucer',
+                family: 'solid',
                 color: 'orange'
             },
             {
-                name: 'horse',
+                //<i class="fa-solid fa-cubes"></i>
+                name: 'Apps and Software',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'cubes',
+                family: 'solid',
                 color: 'orange'
             },
             {
-                name: 'hippo',
+                //<i class="fa-solid fa-laptop"></i>
+                name: 'Gadgets and Stuff',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'laptop',
+                family: 'solid',
                 color: 'orange'
             },
             {
-                name: 'fish',
+                //<i class="fa-solid fa-wrench"></i>
+                name: 'Tutorials and Guides',
                 prefix: 'fa-',
-                type: 'animal',
-                family: 'fas',
+                type: 'wrench',
+                family: 'solid',
                 color: 'orange'
             },
             
         ]
         }
-    }
-
+    },
+   
 }
 </script>
 
@@ -157,9 +105,43 @@ export default {
     padding: 20px 0 50px 0;
 
     .section_card{
-        border: solid 1px #000;
-        width: 100px;
-        height: 100px;
+        justify-content: center;
+        padding: 0 10px;
+
+        .icon {
+            border-top: solid 2px $colorEasternBlue;
+            width: 150px;
+            height: 150px;
+            align-self: center;
+            margin: auto;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            background-color: $colorWhite;
+            &:hover {
+                cursor: pointer;
+            }
+
+            .icon_image {
+                margin-top: 30px;
+                height: 20px;
+                width: 20px;
+                border-radius: 50%;
+                padding: 10%;
+                color: white;
+                background-color: $colorSalmon;
+                //font-size: 3em;
+                
+            }
+            
+            .title {
+                padding: 20px 5px;
+                font-size: small;
+                font-weight: 600;
+
+            }
+
+        }
     }
 }
 
